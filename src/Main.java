fddcb5c3f78e11675b9b897460a7cf66ea85567f
Adam <adam.szanto1@gmail.com>
2023-01-15 16:42:29 +0100

    2022.01.15 - v0.1 Az első Class-okat leraktuk.

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Ticket ticket1 = new Ticket();
+        System.out.println(ticket1.PriorityNum);
+    }
+
+}
---
+++ src/Ticket.java
@@ -0,0 +1,12 @@
+public class Ticket {
+
+    int PriorityNum=0;
+
+    enum Priority {TOP,MID,LOW};
+
+    // Est. time
+    // Owner (default 0?)
+    public Ticket() {
+
+    }
+}
