8570ddf18d5f1759c6bc7d19c0808d23da6e25f1
Adam <adam.szanto1@gmail.com>
2023-01-20 11:49:47 +0100

    2022.01.15 - v0.1 Az első Class-okat leraktuk. 2022.01.20 - v0.2 A CompanyMember Classokat leraktuk.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,24 @@ public class Main {
     public static void main(String[] args) {
         Ticket ticket1 = new Ticket();
         System.out.println(ticket1.PriorityNum);
+
     }
 
 }
+
+
+// User, Manager, Lead. Mindenki tud létrehozni ticketet
+// Ticket: ID, Title, leírás, és 'aktív' állapot  amikor elkészül (+ utána becsült idő és hozzárendelés managerhez).
+// Ticket: Lehet 3 prioritása, TOP 1, MED 2, LOW 3
+// Lehet 3 állapota, 'aktív', 'frissített', 'megoldott'
+// User: Legkevesebb jogköre van, csak létrehozni tud ticketet (+ max frissíteni extra infoval a leírást)
+// Manager: Tud ticketet magához rendelni, becsült időt megadni a tickethez. Ezeknek a hozzárendelt ticketeknek a státuszt állítani, előzményben látható az aktivitás?
+// Manager: Lássa egy helyen az épp még aktív ticketjeit amivel foglalkoznia kell
+// Lead: Tud más managerekhez assignolni ticketet, becsült időt megadni.
+
+
+// Lehet még: Company, Project?
+// Company > Project > Ticket típus
+// Lead: Tud projekteket létrehozni, módosítani. Tud projekthez assignolni & szerepköröket módosítani (User/Manager/Admin) pl.:
+// User1 = Projekt1, Projekt2
+// User2 = Projekt2
--- src/Ticket.java
+++ src/Ticket.java
@@ -1,11 +1,12 @@
-public class Ticket {
+public class Ticket{
 
     int PriorityNum=0;
 
     enum Priority {TOP,MID,LOW};
 
     // Est. time
-    // Owner (default 0?)
+    // Owner (default 0, ?)
+
     public Ticket() {
 
     }
